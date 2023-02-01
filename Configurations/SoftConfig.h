df3cb88b7d537f609e187eaa66cf41d482f0f332
tchoforwilson <tchoforwilson@gmail.com>
2023-02-01 10:08:03 +0100

    Configuration folder

--- Configurations/SoftConfig.h
+++ Configurations/SoftConfig.h
@@ -3,8 +3,10 @@
 
 #include <string>
 
-#define BANK_CODE (std::string("K2")) // constant to identify bank card
-#define TYPE_PIN (std::string("PIN")) // constant for login pin
-#define TYPE_BIO (std::string("BIOMETRIC")) // constant for login biometric
+namespace Configurations{
+    #define BANK_CODE (std::string("K2")) // constant to identify bank card
+    #define TYPE_PIN (std::string("PIN")) // constant for login pin
+    #define TYPE_BIO (std::string("BIOMETRIC")) // constant for login biometric
+}
 
 #endif /* _SOFT_CONFIG_H_ */
